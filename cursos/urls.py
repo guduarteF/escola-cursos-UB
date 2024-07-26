5c71ea694d484c7390ce63ed606076f9d13068c8
Gustavo Duarte Ferreira g <gustavo64duarte@gmail.com>
2024-07-26 11:15:02 -0300

    ViewSets / Routers

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import CursoAPIView,CursosAPIView, AvaliacoesAPIView, AvaliacaoAPIView
+from .views import CursoAPIView,CursosAPIView, AvaliacoesAPIView, AvaliacaoAPIView, AvaliacaoViewSet, CursoViewSet
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register('cursos', CursoViewSet)
+router.register('avaliacoes', AvaliacaoViewSet)
+
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(),name='cursos'),
     path('cursos/<int:pk>', CursoAPIView.as_view(),name='curso'),
-    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
-    path('avaliacoes/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao')
+    path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view(),name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>', AvaliacaoAPIView.as_view(),name='curso_avaliacao'),
+    path('avaliacoes/<int:avaliacao_pk>', AvaliacaoAPIView.as_view(), name='avaliacao'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacao')
 ]
