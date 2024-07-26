396843f0a2aa981775069a1e7f9b9eeb524f5df2
Gustavo Duarte Ferreira g <gustavo64duarte@gmail.com>
2024-07-26 12:01:05 -0300

    Relacionamentos e Paginação

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -20,6 +20,18 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         )
 class CursoSerializer(serializers.ModelSerializer):
 
+    # 1. Nested Relationship
+        # - perfomático -> Pois gera todas as informações de relacionamento no request do GET (ex:tanto de cursos e avaliacões)
+        # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+
+    # 2. Hyperlink Related Field
+        # + perfomático pois ele gera um campo com o hyperlink / endpoint / URI
+        # avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
+
+    # 3. Primary Key Related Field:
+    # ++ perfomático pois ele informa a quantidade apenas , utilizado para sistemas com muitas cargas de acesso/registros
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Curso
         fields = (
@@ -27,5 +39,6 @@ class CursoSerializer(serializers.ModelSerializer):
             'titulo',
             'url',
             'criacao',
-            'ativo'
+            'ativo',
+            'avaliacoes'
         )
--- escola/settings.py
+++ escola/settings.py
@@ -123,3 +123,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
